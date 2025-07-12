bbf1ee92e9ab3df834a3bc9080790a7e45878337
FADEL MUHAMAD RIFAI 411221027 <411221027@mahasiswa.undira.ac.id>
2025-07-12 15:03:51 +0000

    add vercel speed-insights 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
